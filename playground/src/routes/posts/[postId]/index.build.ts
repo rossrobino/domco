290541eb59fd6f2703855d386611074809c86d62
rossrobino <rbrobino@gmail.com>
2023-10-18 09:42:19 -0400

    dynamic paths

--- playground/src/routes/posts/[postId]/index.build.ts
+++ playground/src/routes/posts/[postId]/index.build.ts
@@ -1,17 +1,12 @@
 import { Build } from "domco";
-import { readFile } from "fs/promises";
 
-export const params = [
-	{ postId: "my-post" },
-	// { postId: "another-post" },
-] as const;
+export const params: { postId: string }[] = [];
+
+for (let i = 0; i < 50; i++) {
+	params.push({ postId: `post-${i + 1}` });
+}
 
 export const build: Build<typeof params> = async ({ document }, { params }) => {
 	const h2 = document.querySelector("h2");
 	if (h2) h2.textContent = params.postId;
-	const md = await readFile(
-		"src/lib/content/" + params.postId + ".md",
-		"utf-8",
-	);
-	if (h2) h2.textContent = md;
 };
