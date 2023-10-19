ae707a38031b8bba22292a160bc298bc1e7ee0b6
rossrobino <rbrobino@gmail.com>
2023-10-19 11:49:29 -0400

    clean up

--- playground/src/client.ts
+++ playground/src/client.ts
@@ -7,32 +7,3 @@ import { addBlocks } from "domco";
 customElements.define("share-url", ShareUrl);
 
 await addBlocks(window, [userButton]);
-
-const anchors = document.querySelectorAll("a[href^='/']");
-
-for (const anchor of anchors) {
-	let prefetchTimer: NodeJS.Timeout;
-
-	const listener = (e: Event) => {
-		if (e.target instanceof HTMLAnchorElement) {
-			const href = e.target.href;
-			const isCurrent = href === window.location.href;
-			const isNew = document.querySelector(`link[href='${href}']`) === null;
-			if (isNew && !isCurrent) {
-				prefetchTimer = setTimeout(() => {
-					const link = document.createElement("link");
-					link.rel = "prefetch";
-					link.href = href;
-					document.head.appendChild(link);
-				}, 200);
-			}
-		}
-	};
-
-	const reset = () => clearTimeout(prefetchTimer);
-
-	anchor.addEventListener("mouseover", listener);
-	anchor.addEventListener("focus", listener);
-	anchor.addEventListener("mouseout", reset);
-	anchor.addEventListener("focusout", reset);
-}
