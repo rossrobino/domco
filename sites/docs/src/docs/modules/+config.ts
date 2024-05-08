ce99a7180c10dfb6ce9e5a4f3297d3b679fe03cc
rossrobino <rbrobino@gmail.com>
2024-05-08 09:20:41 -0400

    chore: configure monorepo

--- sites/docs/src/docs/modules/+config.ts
+++ sites/docs/src/docs/modules/+config.ts
@@ -4,7 +4,10 @@ import fs from "node:fs/promises";
 
 export const config: Config = {
 	build: async ({ document }) => {
-		const md = await fs.readFile(`../packages/domco/docs/globals.md`, "utf-8");
+		const md = await fs.readFile(
+			`../../packages/domco/docs/globals.md`,
+			"utf-8",
+		);
 		const { html } = await processMarkdown(md);
 		const article = document.querySelector("article");
 		if (article) article.innerHTML = html;
