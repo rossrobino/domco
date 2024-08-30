3ea7505d88006533a323c996f50e87978ed723bc
rossrobino <rbrobino@gmail.com>
2024-08-30 15:04:43 -0400

    tester back to vercel adapter

--- apps/tester/vite.config.ts
+++ apps/tester/vite.config.ts
@@ -1,19 +1,18 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import react from "@vitejs/plugin-react";
 import { domco } from "domco";
-import { adapter } from "domco/adapter/bun";
+import { adapter } from "domco/adapter/vercel";
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
 		domco({
-			adapter: adapter(),
-			// adapter: adapter({
-			// 	config: {
-			// 		runtime: "nodejs20.x",
-			// 	},
-			// 	isr: { expiration: 60 },
-			// }),
+			adapter: adapter({
+				config: {
+					runtime: "nodejs20.x",
+				},
+				isr: { expiration: 60 },
+			}),
 		}),
 		react(),
 		svelte({
