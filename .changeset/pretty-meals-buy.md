d50685ca025bcb8b05e7cb82e87c785d30006577
rossrobino <rbrobino@gmail.com>
2024-09-12 14:56:09 -0400

    rm default immutable headers

---
+++ .changeset/pretty-meals-buy.md
@@ -0,0 +1,5 @@
+---
+"domco": minor
+---
+
+rm default immutable headers - leave to adapters instead
--- packages/domco/src/app/mw/index.ts
+++ packages/domco/src/app/mw/index.ts
@@ -1,18 +1,9 @@
-import { dirNames, headers } from "../../constants/index.js";
 import type { CreateAppOptions } from "../../types/public/index.js";
 import { setServer } from "../util/index.js";
 import { trimTrailingSlash } from "hono/trailing-slash";
 
 /** Middleware used in `dev` and `prod`. */
 export const standardMiddleware: CreateAppOptions["middleware"] = [
-	{
-		// this can be applied in `dev` because static files are served directly from `src/`
-		path: `/${dirNames.out.client.immutable}/*`,
-		handler: async (c, next) => {
-			await next();
-			c.header("Cache-Control", headers.cacheControl.immutable);
-		},
-	},
 	{
 		path: "/*",
 		handler: setServer,
