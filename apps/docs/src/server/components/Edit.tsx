56afadbefaa7e40da4bfcb1fa0984ef644e03dee
rossrobino <rbrobino@gmail.com>
2024-09-18 08:57:02 -0400

    docs: edit link

--- apps/docs/src/server/components/Edit.tsx
+++ apps/docs/src/server/components/Edit.tsx
@@ -3,7 +3,7 @@ import type { FC } from "hono/jsx";
 export const Edit: FC = () => {
 	return (
 		<a
-			href="https://github.com/rossrobino/domco/tree/main/apps/docs/src/content"
+			href="https://github.com/rossrobino/domco/tree/main/apps/docs/src/server/content"
 			class="button secondary inline-flex"
 		>
 			Edit this page
