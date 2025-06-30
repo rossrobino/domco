97ca8e818407c3e80cea5dcabfea37d5e43a6869
rossrobino <rbrobino@gmail.com>
2025-06-30 14:13:15 -0400

    version

--- packages/domco/src/version/index.ts
+++ packages/domco/src/version/index.ts
@@ -1,4 +1,4 @@
 // this file is generated in scripts/version/index.ts
 // this might be out of sync with github until the project is built and committed.
 // npm will have the correct version.
-export const version = "4.1.7";
+export const version = "4.2.0";
