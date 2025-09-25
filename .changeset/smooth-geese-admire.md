af048ccad1e4e339a4512870fe36e7c769185e74
rossrobino <rbrobino@gmail.com>
2025-09-25 14:45:15 -0400

    patch: update `create` deps

---
+++ .changeset/smooth-geese-admire.md
@@ -0,0 +1,5 @@
+---
+"create-domco": patch
+---
+
+patch: Bump `create-domco` dependencies.
--- packages/create-domco/src/dependencies/index.ts
+++ packages/create-domco/src/dependencies/index.ts
@@ -3,13 +3,13 @@ import { version } from "domco/version";
 export const getDependencies = () => {
 	return {
 		domco: `${version.split(".").at(0)}.0.0`,
-		typescript: "5.8.0", // typescript does not follow semver
+		typescript: "5.9.0", // typescript does not follow semver
 		vite: "7.0.0",
 
 		// framework
 		hono: "4.0.0",
 		ovr: "4.0.0",
-		h3: "2.0.0-beta.1",
+		h3: "2.0.0-beta.4",
 		elysia: "1.0.0",
 		elysiaHtml: "1.0.0",
 		"mono-jsx": "0.6.0", // minor since pre v1
